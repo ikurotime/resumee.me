45b26dc5dd9117844c5d01284c93c230cedc96b7
David <davidhuertasortiz@gmail.com>
2024-09-21 17:21:20 +0200

    Delete missing import

--- app/cv-builder/page.tsx
+++ app/cv-builder/page.tsx
@@ -1,7 +1,5 @@
-import { addBlock, getWebsite } from '@/actions/websites'
-
 import { ClientWrapper } from '@/components/ClientWrapper'
-import DraggableBlock from '@/components/DraggableBlock'
+import { getWebsite } from '@/actions/websites'
 
 export default async function CVBuilderPage({
   params
@@ -14,11 +12,7 @@ export default async function CVBuilderPage({
     <ClientWrapper>
       <div>
         <h1>{website?.title}</h1>
-        <div>
-          {website?.blocks.map((block) => (
-            <DraggableBlock key={block.id} block={block} onUpdate={addBlock} />
-          ))}
-        </div>
+        <div></div>
         {/* Add Block form */}
       </div>
     </ClientWrapper>
